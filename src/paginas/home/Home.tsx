7288891a0ae4a336760b6397c72c2a25038d1b68
Rafael <rafaelproinfo@gmail.com>
2022-01-23 02:20:18 -0300

    Criar Componente Navbar

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,22 +1,11 @@
 import React from "react";
-import { Box, Button, Paper } from "@material-ui/core";
+import { Typography, Box, Grid, Button } from '@material-ui/core';
 import './Home.css';
 
 function Home() {
   return (
     <>
-      <Paper>
-        <Box p={2}>
-          <Box display="flex" justifyContent="center">
-            <h1>Título</h1>
-          </Box>
-          <img src="https://i.imgur.com/IVCDEHU.png" alt="Imagem" style={{ width:"100%", height:"100%"}}/>
-          <Box display="flex" justifyContent="center" p={2}>
-            <Button variant="contained" color="primary">Texto 1</Button>
-            <Button variant="contained" color="secondary">Texto 2</Button>
-          </Box>
-        </Box>
-      </Paper>
+      <h1>Home</h1>
     </>
   );
 }
