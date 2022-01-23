2980be431096cceca79d991a02435312c147921a
Rafael <rafaelproinfo@gmail.com>
2022-01-23 01:43:15 -0300

    Flex Box no Material UI

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,23 +1,22 @@
 import React from "react";
-import { Grid, Paper } from "@material-ui/core";
+import { Box, Button, Paper } from "@material-ui/core";
 import './Home.css';
 
 function Home() {
   return (
     <>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={8}>
-          <Paper style={{ height: "100vh", background: "lightgrey"}}/>
-        </Grid>
-        <Grid item container direction="column" xs={12} sm={4} spacing={2}>
-          <Grid item>
-            <Paper style={{ height: "49vh", background: "orange"}}/>
-          </Grid>
-          <Grid item>
-            <Paper style={{ height: "49vh", background: "green"}}/>
-          </Grid>
-        </Grid>
-      </Grid>
+      <Paper>
+        <Box p={2}>
+          <Box display="flex" justifyContent="center">
+            <h1>Título</h1>
+          </Box>
+          <img src="https://i.imgur.com/IVCDEHU.png" alt="Imagem" style={{ width:"100%", height:"100%"}}/>
+          <Box display="flex" justifyContent="center" p={2}>
+            <Button variant="contained" color="primary">Texto 1</Button>
+            <Button variant="contained" color="secondary">Texto 2</Button>
+          </Box>
+        </Box>
+      </Paper>
     </>
   );
 }
