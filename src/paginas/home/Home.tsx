ac30a6d6f4be1d0a768ec69605a08b3b0e8b221d
Rafael <rafaelproinfo@gmail.com>
2022-01-23 00:18:20 -0300

    Flex Grid no Material UI

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { Grid, Paper } from "@material-ui/core";
 import './Home.css';
 
 function Home() {
   return (
     <>
-      <h1 className="titulo">Home</h1>
-      <img src="https://i.imgur.com/7NXtjG4.png" alt="Imagem Tela Inicial" className="img"/>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={8}>
+          <Paper style={{ height: "100vh", background: "lightgrey"}}/>
+        </Grid>
+        <Grid item container direction="column" xs={12} sm={4} spacing={2}>
+          <Grid item>
+            <Paper style={{ height: "49vh", background: "orange"}}/>
+          </Grid>
+          <Grid item>
+            <Paper style={{ height: "49vh", background: "green"}}/>
+          </Grid>
+        </Grid>
+      </Grid>
     </>
   );
 }
