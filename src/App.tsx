41dba22ffb88bf1fe0a2865e8bef0f9db190c6ed
Rafael <rafaelproinfo@gmail.com>
2022-01-27 00:59:28 -0300

    Criar Router Home

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,27 @@ import Footer from './components/estaticos/footer/Footer';
 import { Grid } from '@material-ui/core';
 import Home from './paginas/home/Home';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+/* Instalar o React Router:
+  yarn add react-router-dom@5.3.0
+  yarn add @types/react-router-dom@5.1.8
+*/
 
 function App() {
   return (
     <>
+      <Router>
       <Navbar />
-      <Home />
+        <Switch>
+          <div>
+            <Route path='/home'>
+              <Home />
+            </Route>
+          </div>
+        </Switch>
       <Footer />
+    </Router>
     </>
   );
 }
