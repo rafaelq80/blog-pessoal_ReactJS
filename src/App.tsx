5497adb71fb089b6ad1455d0b1d96286369b4adb
Rafael <rafaelproinfo@gmail.com>
2022-01-29 00:23:35 -0300

    Refatoração CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,27 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
 import Home from './paginas/home/Home';
-import './App.css';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './paginas/login/Login';
+import './App.css';
 
-/* Instalar o React Router:
-  yarn add react-router-dom@5.3.0
-  yarn add @types/react-router-dom@5.1.8
-*/
 
 function App() {
   return (
-    <>
-      <Router>
+    <Router>
       <Navbar />
         <Switch>
-          <div style={{minHeight:'100vh'}}>
-          <Route exact path='/'>
+          <div style={{minHeight: '100vh'}}>
+
+            <Route exact path='/'>
               <Login />
             </Route>
 
-          <Route path='/login'>
+            <Route path='/login'>
               <Login />
             </Route>
+
             <Route path='/home'>
               <Home />
             </Route>
@@ -32,7 +29,6 @@ function App() {
         </Switch>
       <Footer />
     </Router>
-    </>
   );
 }
 
