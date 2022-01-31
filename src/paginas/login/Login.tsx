b5266424fd2394ac37ddc85d685cdafe961e5b2c
Rafael <rafaelproinfo@gmail.com>
2022-01-31 15:02:53 -0300

    Configurar Form

--- src/paginas/login/Login.tsx
+++ src/paginas/login/Login.tsx
@@ -1,24 +1,53 @@
-import React from 'react';
+import React, { useState, ChangeEvent} from 'react';
 import { Grid, Box, Typography, TextField, Button } from '@material-ui/core';
 import { Link } from 'react-router-dom';
 import './Login.css';
+import UserLogin from '../../models/UserLogin';
 
 function Login() {
 
+
+    const [userLogin, setUserLogin] = useState<UserLogin>(
+        {
+            id: 0,
+            nome: '',
+            usuario: '',
+            senha: '',
+            foto: '',
+            token: ''
+        }
+        )
+
+        /**
+         * 
+         * @param e -> Captura os valores do input e insere na função setUserLogin
+         */
+        function updatedModel(e: ChangeEvent<HTMLInputElement>) {
+
+            setUserLogin({
+                ...userLogin,
+                [e.target.name]: e.target.value
+            })
+        }
+
+        async function onSubmit(e: ChangeEvent<HTMLFormElement>){
+            e.preventDefault(); //Previne o comportamento padrão do formulário (Atualizar a página)
+
+            console.log('userLogin: ' + Object.values(userLogin)); //Imprime o objeto userLogin no Console
+        }
+       
     return (
         <Grid container direction='row' justifyContent='center' alignItems='center'>
             <Grid alignItems='center' xs={6}>
                 <Box paddingX={20}>
-                    <form>
+                    <form onSubmit={onSubmit}>
                         <Typography variant='h3' gutterBottom color='textPrimary' component='h3' align='center' className='textos1'>Entrar</Typography>
-                        <TextField id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
-                        <TextField id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
+                        <TextField value={userLogin.usuario} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='usuario' label='usuário' variant='outlined' name='usuario' margin='normal' fullWidth />
+                        <TextField value={userLogin.senha} onChange={(e:ChangeEvent<HTMLInputElement>) => updatedModel(e)} id='senha' label='senha' variant='outlined' name='senha' margin='normal' type='password'fullWidth />
                         <Box marginTop={2} textAlign='center'>
-                            <Link to='/home' className='text-decorator-none'>
-                                <Button type='submit' variant='contained' color='primary'>
-                                    Logar
-                                </Button>
-                            </Link>
+                            <Button type='submit' variant='contained' color='primary'>
+                                Logar
+                            </Button>
                         </Box>
                     </form>
                     <Box display='flex' justifyContent='center' marginTop={2}>
---
+++ src/services/Service.ts
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const api = axios.create({
+    baseURL: 'https://bp2022.herokuapp.com'
+})
+
+/**
+ * 
+ * @param url -> Url do recurso
+ * @param dados -> Objeto JSON da Requisição 
+ * @param setDado -> Objeto JSON de Resposta
+ * async -> Função assincrona que espera a resposta do servidor para continuar a execução
+ * api.post -> Método Post
+ * setDado -> Inseres a Resposta na variável setDado
+ */
+
+export const login = async(url: any, dados: any, setDado: any) => {
+    const resposta = await api.post(url, dados)
+    setDado(resposta.data)
+}
